389b84277e8d553d5a1d4c8038906cba22b51cea
Andy Phillips <44531197+andyrphillips@users.noreply.github.com>
2025-02-27 09:32:04 -0500

    attempting more obvious vulnerabilities

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ from flask import request
 
 app = flask.Flask(__name__)
 
-@app.route('/search')
-def search():
-    # SECURITY ISSUE: Command injection vulnerability
-    query = request.args.get('q', '')
-    result = subprocess.call("grep -r " + query + " /app/data", shell=True)
-    return str(result)
+@app.route('/exec')
+def execute_command():
+    # VERY obvious command injection
+    cmd = request.args.get('cmd')
+    os.system(cmd)  # This should definitely be caught
+    return "Command executed"
 
-@app.route('/user/<username>')
-def user_profile(username):
-    # SECURITY ISSUE: Potential path traversal
-    with open(f"/app/data/{username}", "r") as f:
+@app.route('/read')
+def read_file():
+    # VERY obvious path traversal
+    filename = request.args.get('file')
+    with open(filename, 'r') as f:  # This should definitely be caught
         return f.read()
 
 if __name__ == '__main__':
