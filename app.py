1c7f0ebf56b41ec3b77930278eb30a5c00b4634a
Andy Phillips <44531197+andyrphillips@users.noreply.github.com>
2025-02-27 12:58:25 -0500

    uncomment

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ from flask import request
 
 app = flask.Flask(__name__)
 
-# @app.route('/exec')
-# def execute_command():
-#     # VERY obvious command injection
-#     cmd = request.args.get('cmd')
-#     os.system(cmd)  # This should definitely be caught 
-#     return "Command executed"
+@app.route('/exec')
+def execute_command():
+    # VERY obvious command injection
+    cmd = request.args.get('cmd')
+    os.system(cmd)  # This should definitely be caught 
+    return "Command executed"
 
-# @app.route('/read')
-# def read_file():
-#     # VERY obvious path traversal
-#     filename = request.args.get('file')
-#     with open(filename, 'r') as f:  # This should definitely be caught
-#         return f.read()
+@app.route('/read')
+def read_file():
+    # VERY obvious path traversal
+    filename = request.args.get('file')
+    with open(filename, 'r') as f:  # This should definitely be caught
+        return f.read()
 
 if __name__ == '__main__':
     app.run(debug=True)
